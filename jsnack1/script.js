657c6befc4269376878cdd3cace404b405463246
antoninodangelo11 <reapercsp@gmail.com>
2023-04-18 15:08:29 +0200

    end jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,3 +2,25 @@
 Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.*/
+
+let num;
+let myArr = [];
+let valid = 0;
+
+
+for (let i = 0; i < 6; i++) {
+    num = parseInt(prompt('Inserisci un numero'));
+
+    if (num % 2 == 0) {
+        console.log('pari', num);
+    } 
+    
+    else if (num % 2 == 1) {
+        valid = 1;
+        console.log('dispari', num);
+        parseInt(myArr.push(num));
+    }
+    
+}
+
+console.log('MyArr', myArr);
